443ef4b6e528010e90a43a207bafbad9c89f9d35
Mateus Andreatta <mateus-henrique2001@hotmail.com>
2022-04-19 19:59:47 -0300

    Adicionado querys no repositorio para trazer a lista de condominios de um usuario por ID ou por DOCUMENTO

--- src/main/java/br/com/meucampestre/meucampestre/repositories/CondominioRepo.java
+++ src/main/java/br/com/meucampestre/meucampestre/repositories/CondominioRepo.java
@@ -2,6 +2,15 @@ package br.com.meucampestre.meucampestre.repositories;
 
 import br.com.meucampestre.meucampestre.domain.models.Condominio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CondominioRepo extends JpaRepository<Condominio, Long> {
+
+    @Query(value = "SELECT cond.* FROM condominio cond, condominio_usuarios condU, usuario u WHERE condU.condominio_id = cond.id AND condU.usuarios_id = u.id AND u.id = ?1", nativeQuery = true)
+    List<Condominio> getCondominiosPorIdUsuario(String usuarioId);
+
+    @Query(value = "SELECT cond.* FROM condominio cond, condominio_usuarios condU, usuario u WHERE condU.condominio_id = cond.id AND condU.usuarios_id = u.id AND u.documento = ?1", nativeQuery = true)
+    List<Condominio> getCondominiosPorDocumentoUsuario(String documentoUsuario);
 }
