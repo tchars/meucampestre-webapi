6e185ac5bd89e89f8b645af31efc810eca1f09ff
Charles Jamil <char.r@hotmail.com>
2022-05-30 21:41:25 -0300

    ajusta configs

--- src/main/java/br/com/meucampestre/meucampestre/MeuCampestreApplication.java
+++ src/main/java/br/com/meucampestre/meucampestre/MeuCampestreApplication.java
@@ -26,31 +26,30 @@ public class MeuCampestreApplication {
 	@Bean
 	CommandLineRunner run(UsuarioService usuarioService){
 		return args -> {
+			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.BACKOFFICE, null, null));
+			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.SINDICO,null,null));
+			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.CONSELHEIRO, null,null));
+			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.PORTEIRO, null,null));
+			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.MORADOR, null, null));
 
-//			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.BACKOFFICE, null, null));
-//			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.SINDICO,null,null));
-//			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.CONSELHEIRO, null,null));
-//			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.PORTEIRO, null,null));
-//			usuarioService.salvarPapel(new Papel(null, TiposDePapeis.MORADOR, null, null));
-//
-//			usuarioService.salvarUsuario(
-//					new Usuario(
-//							null,
-//							"Back Office - Meu Campestre",
-//							"bo@meucampestre.com.br",
-//							"123123",
-//							"00011122233",
-//							"4133331234",
-//							"https://avatars.dicebear" +
-//									".com/v2/identicon/4bcd5df34f59686b680f036d26b31277.svg",
-//							true,
-//							null,
-//							null,
-//							null
-//					)
-//			);
-//
-//			usuarioService.adicionarPapelAoUsuario("00011122233", TiposDePapeis.BACKOFFICE);
+			usuarioService.salvarUsuario(
+					new Usuario(
+							null,
+							"Back Office - Meu Campestre",
+							"bo@meucampestre.com.br",
+							"123123",
+							"00011122233",
+							"4133331234",
+							"https://avatars.dicebear" +
+									".com/v2/identicon/4bcd5df34f59686b680f036d26b31277.svg",
+							true,
+							null,
+							null,
+							null
+					)
+			);
+
+			usuarioService.adicionarPapelAoUsuario("00011122233", TiposDePapeis.BACKOFFICE);
 		};
 	}
 }
